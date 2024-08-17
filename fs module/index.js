24fb104b810c2f52d8bd6294614b49cdce2a707e
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-08-17 21:46:56 +0530

    Update index.js

--- fs module/index.js
+++ fs module/index.js
@@ -1,52 +1,78 @@
 const fs = require("fs");
+const prompt = require("prompt-sync")();
+console.log(
+  "1) create File \n 2) read file \n 3) rename file \n 4) Update file \n 5) delete file"
+);
+let choice = prompt();
 
-//   fs.writeFile("index.html","purv", (err) => {
-//     if (err) {
-//       console.log(err);
+switch (choice) {
+  case "1":
+    console.log("enter file name :- ");
+    let fileName = prompt();
+    console.log("enter code");
+    let newCode = prompt();
 
-//     } else {
-//       console.log(
-//         "File created successfully! Now opening in default browser..."
-//       )
-//     }
-//   });
+    fs.writeFile(fileName, newCode, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(
+          "File created successfully! Now opening in default browser..."
+        );
+      }
+    });
+    break;
 
+  case "2":
+    console.log("enter file name :- ");
+    let read_fileName = prompt();
+    fs.readFile(read_fileName, "utf8", (err, data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(data)
+      }
+    });
+    break;
 
+  case "3":
+    console.log("enter file name :- ");
+    let update_fileName = prompt();
+    console.log("enter code");
+    let new_filename = prompt();
 
-// fs.readFile("index.html", "utf8", (err, data) => {
-//   if (err) {
-//     console.log(err); 
-//   } else {
-//     console.log(data);
-//   }
-// });
+    fs.rename(update_fileName, new_filename, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Renamed successfully!");
+      }
+    });
+    break;
 
+  case "4":
+    console.log("enter file name :- ");
+    let Contant_fileName = prompt();
+    console.log("enter code");
+    let contant = prompt();
+    fs.appendFile(Contant_fileName, contant, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Content appended successfully!");
+      }
+    });
+    break;
 
+  case "5":
+    console.log("enter file name :- ");
+    let delete_fileName = prompt();
 
-// fs.rename("index.html", "purv.html", (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Renamed successfully!");
-//   }
-// });
-
-
-
-// fs.appendFile("purv.html","\n This is the new content", (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Content appended successfully!");
-//     }
-// })
-
-
-// fs.unlink("purv.html", (err) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else{
-//             console.log("File deleted successfully!");
-//         }
-// })
+    fs.unlink(delete_fileName, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File deleted successfully!");
+      }
+    });
+}
