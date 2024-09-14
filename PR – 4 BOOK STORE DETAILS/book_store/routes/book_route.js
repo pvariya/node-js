ca60d052cf5f27b8d8ff72ea4c8abb108faaa88c
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-09-14 15:39:01 +0530

    Update book_route.js

--- PR – 4 BOOK STORE DETAILS/book_store/routes/book_route.js
+++ PR – 4 BOOK STORE DETAILS/book_store/routes/book_route.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
-const { getBookById, deleteBook, getAllBooks, addBook } = require('../controllers/bookController')
-
+const { getBookById, deleteBook, getAllBooks, addBook ,uploade, updateBook} = require('../controllers/bookController')
 const bookRoute = Router()
 
 bookRoute.get('/book/:id', getBookById)
-bookRoute.delete('/delete/:id',deleteBook)
-bookRoute.get('/getBook/',getAllBooks)
-bookRoute.post('/postBook',addBook)
+bookRoute.delete('/delete/:id', deleteBook)
+bookRoute.get('/getBook/', getAllBooks)
+bookRoute.post('/postBook', uploade.single('img'), addBook)
+bookRoute.patch('/update/:id',updateBook);
+
 module.exports = bookRoute;
