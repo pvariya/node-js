bc47be6ce1807643cd915a1b23825ee5475ec30f
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-09-14 15:38:41 +0530

    Update index.js

--- PR – 4 BOOK STORE DETAILS/book_store/index.js
+++ PR – 4 BOOK STORE DETAILS/book_store/index.js
@@ -4,18 +4,20 @@ const app = express();
 const path = require('path');
 const dbConnect = require('./config/db');
 const bookRoute = require('./routes/book_route');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8090;
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/uploads",express.static(path.join(__dirname, 'uploads')));
+
 
 
 app.get('/', (req, res) => {
   res.send("welcome to the book store");
 })
-
 
 app.use('/books',bookRoute)
 
@@ -23,3 +25,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   dbConnect()
 });
+
