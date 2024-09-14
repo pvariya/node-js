8a32d36cc27900ad6b00759613f0cd20b388c836
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-09-14 15:39:19 +0530

    Update book_schema.js

--- PR – 4 BOOK STORE DETAILS/book_store/models/book_schema.js
+++ PR – 4 BOOK STORE DETAILS/book_store/models/book_schema.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+    const mongoose = require('mongoose')
 
-const bookSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    category: String,
-    publicationYear: Number,
-    price: Number,
-    quantity: Number,
-    description: String,
-    imageUrl: String
-})
+    const bookSchema = mongoose.Schema({
+        title: String,
+        author: String,
+        category: String,
+        publicationYear: Number,
+        price: Number,
+        quantity: Number,
+        description: String,
+        imageUrl: String
+    })
 
-const book = mongoose.model('book', bookSchema)
-module.exports = book
+    const book = mongoose.model('book', bookSchema)
+    module.exports = book
