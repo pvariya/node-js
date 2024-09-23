854b0eabebc7001cbb4ad7cda0c5a15792148309
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-09-23 19:21:35 +0530

    Delete exam_7 directory

--- exam_7/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbConnetc=async()=>{
-    await mongoose.connect("mongodb://localhost:27017/exam-7")
-    console.log("Connected to Mongo")   
-}
-
-module.exports=dbConnetc
